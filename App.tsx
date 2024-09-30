99d62c13ca81ac5c86e33932386c474efc428631
Thamoddya Rashmitha <thamoddyarashmithadissanayake@gmail.com>
2024-09-30 14:25:21 +0530

    Update

--- App.tsx
+++ App.tsx
@@ -1,15 +1,33 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
 } from 'react-native';
+import SplashScreen from './src/screens/splash/SplashScreen';
+import HomeScreen from './src/screens/home/HomeScreen';
 
-
+const Stack = createNativeStackNavigator();
 function App() {
   return (
-    <SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen
+          name="Splash"
+          component={SplashScreen}
+          options={{ headerShown: false }}
+        />
 
-    </SafeAreaView>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
   );
 }
 
